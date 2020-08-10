290ceea2ee630f8a9cc9d64712702b806d5d7e33
Fppk <54667329+Fppk@users.noreply.github.com>
2020-08-10 17:29:47 +0800

    Update mathpi_slow.cpp

--- mathpi_slow.cpp
+++ mathpi_slow.cpp
@@ -4,12 +4,11 @@ int main()
 {
 	register unsigned long long int count = 1;
 	register double pi = 4.0;
-	for (; pi != 3.141592653589793; count++)
+	for (;; count++)
 	{
 		pi -= 4.0/(4*count-1);
 		pi += 4.0/(4*count+1);
 		printf("result of %lld:%.15lf\n", count, pi);
 	}
-	printf("\n================\nOK!\nruntimes:%lld\tresult:%.15ld\n================\n", count, pi);
 	return 0;
 }
